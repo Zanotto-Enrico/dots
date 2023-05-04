ca43424fc3c0f40462cffbc6548b61a107ae42a8
Zanotto-Enrico <887566@stud.unive.it>
2023-05-04 13:33:47 +0200

    added nmtui and eduroam scripts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 	alias tmux='tmux -u'
+	alias eduroam="$HOME/scripts/eduroam.sh"
+	alias nmtui="$HOME/scripts/nmtui.sh"
 end
 
 set -U fish_greeting 
