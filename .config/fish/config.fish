9914c0ea012bec30c4e2a601cf33937da80f615e
Zanotto-Enrico <887566@stud.unive.it>
2024-03-16 17:52:23 +0100

    waybar update fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
 	alias tmux='tmux -u'
 	alias eduroam="$HOME/scripts/eduroam.sh"
 	alias nmtui="$HOME/scripts/nmtui.sh"
+    alias matrix="unimatrix -lp -s 95"
 end
 
 set -U fish_greeting 
